e3a55de85294a02614c5721ae7650a8a6c1b7827
Maximillian <maxim00795@mail.ru>
2017-05-27 18:16:39 +0300

    Interfaces edit

--- Interfaces/MathInterfaces.cs
+++ Interfaces/MathInterfaces.cs
@@ -11,8 +11,9 @@ namespace Allax
     public interface IBlock
 	{
 		void Init(List<byte> arg); // P-Block: "1, 8 , 4, 3, 5, 7, 6, 2" array of char where char is num, not text.
-											// S-Block. First block 4x4: " 0x0A, 0x03, 0x03, ... , 0x01" (Length=16). Second block 4x4: "0x01, 0x02, 0xF, ..., 0x02, 0x01" (Length=16)
-	}
+                                   // S-Block. First block 4x4: " 0x0A, 0x03, 0x03, ... , 0x01" (Length=16). Second block 4x4: "0x01, 0x02, 0xF, ..., 0x02, 0x01" (Length=16)
+        List<SBlockState> ExtractStates(Int64 Min, Int64 CurrentCor, int inputs);
+    }
 	public interface ILayer
 	{
         LayerType GetLayerType();
--- MathScript.cs
+++ MathScript.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 namespace Allax
 {
+    
     class SBlockDB : ISBlockDB
     {
         //Fourier Transform DB
@@ -90,6 +92,7 @@ namespace Allax
     abstract class Block : IBlock
     {
         public abstract void Init(List<byte> arg); //SBlock size:4-8. Whole word: up to 64bit.
+        public abstract List<SBlockState> ExtractStates(Int64 Min, Int64 CurrentCor, int inputs);
     };
     class PBlock : Block
     {
@@ -120,12 +123,20 @@ namespace Allax
                 }
             }
         }
+        public override List<SBlockState> ExtractStates(long Min, long CurrentCor, int inputs)
+        {
+            throw new NotImplementedException();
+        }
     }
     class KBlock : Block
     {
         public override void Init(List<byte> arg)
         {
             throw new NotImplementedException("KBlock do not needed to init");
+        }
+        public override List<SBlockState> ExtractStates(long Min, long CurrentCor, int inputs)
+        {
+            throw new NotImplementedException();
         }
     }
     class SBlock : Block
@@ -137,7 +148,7 @@ namespace Allax
         int VarCount;
         byte _length;
         string BlockID = "";
-        public List<SBlockState> ExtractStates(Int64 MIN, Int64 CurrentCor, int inputs)// MIN prevalence, current inputs
+        public override List<SBlockState> ExtractStates(Int64 MIN, Int64 CurrentCor, int inputs)// MIN prevalence, current inputs
         {
             var States = new List<SBlockState>();
             foreach (SBlockState state in _states)
@@ -178,7 +189,7 @@ namespace Allax
             if (arg.Count >= bias)
             {
                 if (arg.Count - bias != 0)
-                    VarCount = (int)System.Math.Log(arg.Count - bias, 2); // Table: 2**n x n
+                    VarCount = (int)Math.Log(arg.Count - bias, 2); // Table: 2**n x n
                 if (VarCount == _length)
                 {
                     FuncTable.AddRange(Enumerable.Repeat<List<byte>>(new List<byte>(), arg.Count - bias));
@@ -211,7 +222,6 @@ namespace Allax
                 }
             }
         }
-
         void SetDB(SBlockDB database)
         {
             _database = database;
@@ -332,7 +342,7 @@ namespace Allax
             throw new NotImplementedException();
         }
     }
-    struct SBlockState
+    public struct SBlockState
     {
         public SBlockState(Int64 cor, int inputs, int outputs)
         {
@@ -369,13 +379,6 @@ namespace Allax
         {
             return Layers.ConvertAll(x => ((ILayer)x));
         }
-
-        public virtual void PerformLinearAnalisys()
-        {
-            //!!
-            //NOT IMPLEMENTED
-
-        }
         public SPNet(SPNetSettings settings)
         {
             _settings = settings;
@@ -409,14 +412,20 @@ namespace Allax
                     }
             }
         }
+        public virtual void PerformLinearAnalisys()
+        {
+            throw new NotImplementedException();
+
+        }
     }
-    public class WayConverter
+    public static class WayConverter
     {
-        bool CheckStandartNetCondition(ISPNet Net)
+        //need to implement. True as default.
+        public static bool CheckStandartNetCondition(ISPNet Net)
         {
             return true;
         }
-        public SPNetWay ToEmptyWay(ISPNet Net)
+        public static SPNetWay ToEmptyWay(ISPNet Net)
         {
             var NetWay = new SPNetWay();
             if(CheckStandartNetCondition(Net))
@@ -435,8 +444,8 @@ namespace Allax
                         var tmp_block = new SPNetWayBlock();
                         tmp_block.active_inputs = new List<bool>();
                         tmp_block.active_outputs = new List<bool>();
-                        tmp_block.active_inputs.AddRange(Enumerable.Repeat<bool>(false, Settings.word_length));
-                        tmp_block.active_outputs.AddRange(Enumerable.Repeat<bool>(false, Settings.word_length));
+                        tmp_block.active_inputs.AddRange(Enumerable.Repeat(false, Settings.word_length));
+                        tmp_block.active_outputs.AddRange(Enumerable.Repeat(false, Settings.word_length));
                         tmp.blocks.Add(tmp_block);
                     }
                     else
@@ -446,8 +455,8 @@ namespace Allax
                             var tmp_sblock = new SPNetWayBlock();
                             tmp_sblock.active_inputs = new List<bool>();
                             tmp_sblock.active_outputs = new List<bool>();
-                            tmp_sblock.active_inputs.AddRange(Enumerable.Repeat<bool>(false, Settings.word_length / Settings.sblock_count));
-                            tmp_sblock.active_outputs.AddRange(Enumerable.Repeat<bool>(false, Settings.word_length / Settings.sblock_count));
+                            tmp_sblock.active_inputs.AddRange(Enumerable.Repeat(false, Settings.word_length / Settings.sblock_count));
+                            tmp_sblock.active_outputs.AddRange(Enumerable.Repeat(false, Settings.word_length / Settings.sblock_count));
                             tmp.blocks.Add(tmp_sblock);
                         }
                     }
@@ -461,10 +470,238 @@ namespace Allax
                 throw new Exception("Azaza. Wrong net.");
             }
         }
+        public static SPNetWay CloneWay(SPNetWay Way, bool empty = false)
+        {
+            SPNetWay NewWay = new SPNetWay();
+            if(Way.layers!=null)
+            {
+                NewWay.layers = new List<SPNetWayLayer>(Way.layers.Count);
+                for(int i=0;i<Way.layers.Count;i++)
+                {
+                    var tmp_layer = new SPNetWayLayer();
+                    tmp_layer.type = Way.layers[i].type;
+                    if(Way.layers[i].blocks!=null)
+                    {
+                        for(int j=0;j<Way.layers[i].blocks.Count;j++)
+                        {
+                            var tmp_block = new SPNetWayBlock();
+                            if(Way.layers[i].blocks[j].active_inputs!=null& Way.layers[i].blocks[j].active_outputs!=null)
+                            {
+                                tmp_block.active_inputs = new List<bool>(Way.layers[i].blocks[j].active_inputs.Count);
+                                tmp_block.active_outputs = new List<bool>(Way.layers[i].blocks[j].active_outputs.Count);
+                                tmp_block.active_inputs.AddRange(Enumerable.Repeat(false, Way.layers[i].blocks[j].active_inputs.Count));
+                                tmp_block.active_outputs.AddRange(Enumerable.Repeat(false, Way.layers[i].blocks[j].active_outputs.Count));
+                                if (!empty&(Way.layers[i].blocks[j].active_inputs.Count== Way.layers[i].blocks[j].active_outputs.Count))
+                                {
+                                    for (int k = 0; k < Way.layers[i].blocks[j].active_inputs.Count; k++)
+                                    {
+                                        tmp_block.active_inputs[k] = Way.layers[i].blocks[j].active_inputs[k];
+                                        tmp_block.active_outputs[k] = Way.layers[i].blocks[j].active_outputs[k];
+                                    }
+                                }
+
+                            }
+                            tmp_layer.blocks.Add(tmp_block);
+                        }
+                    }
+                    NewWay.layers.Add(tmp_layer);
+                }
+            }
+            return NewWay;
+        }
+        public static SPNetWay ToWay(ISPNet Net, OpenTextInput Input)
+        {
+            throw new NotImplementedException();
+        }
+        public static byte ToByte(List<bool> Input)
+        {
+            throw new NotImplementedException();
+            byte ret = 0;
+
+            return ret;
+        }
+        public static List<bool> ToList(int Input, int length)
+        {
+            throw new NotImplementedException();
+            var ret = new List<bool>(length);
+
+            return ret;
+        }
+    }
+    public struct MultiTime
+    {
+        Int64 UnixTime;
+        Int64 Ticks;
+    }
+    public struct SolverParams
+    {
+        int Weight;
+        MultiTime Time;
+    }
+    public struct Task
+    {
+        SPNetWay Way;
+        SolverParams Params;
+        public Task(SPNetWay Way, SolverParams Params)
+        {
+            this.Way = Way;
+            this.Params = Params;
+        }
     }
     public class TaskConstructor
     {
-
+        int _rounds_count;
+        int MIN;
+        ISPNet _net;
+        Queue<Task> _tasks;
+        SPNetWay _tempEmptyWay;
+        OpenTextInputWeightIterator Iter;
+        public TaskConstructor(ISPNet Net)
+        {
+            _net = Net;
+            _rounds_count = _net.GetLayers().Count / 3;
+            _tempEmptyWay = WayConverter.ToEmptyWay(_net);
+        }
+        void AnalysePreviousTasks()
+        {
+            throw new NotImplementedException();
+        }
+        public Queue<Task> GetTasks(int count)
+        {
+            if(_tasks.Count!=0)
+            {
+                AnalysePreviousTasks();
+            }
+            _tasks.Clear();
+            for(int i=0;(i<count)&&Iter.IsFinished();i++)
+            {
+                OpenTextInput NextInput=Iter.NextState();
+                SPNetWay ws = WayConverter.ToWay(_net, NextInput);
+                _tasks.Enqueue(new Task(ws, new SolverParams()));
+            }
+            return _tasks;
+        }
+    }
+    class OpenTextInputWeightIterator
+    {
+        List<OpenTextInputTextBlock> Blocks;
+        public OpenTextInput NextState()
+        {
+            throw new NotImplementedException();
+        }
+        public bool IsFinished()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class OpenTextInputTextBlock
+    {
+        List<byte> input;
+        int weight;
+        bool finished;
+        int StatesPassed;
+        public List<byte> NextState()
+        {
+            throw new NotImplementedException();
+        }
+        public bool IsFinished()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public struct OpenTextInput
+    {
+        List<byte> input;
+        int weight;
+    }
+    public static class BaseSolver
+    {
+        static int SearchLastNotEmptyLayer(SPNetWay Way)
+        {
+            int index = -1;
+            for (int i = 0; i < Way.layers.Count; i++)
+            {
+                int input_sum = 0;
+                if (Way.layers[i].blocks != null)
+                    foreach (var block in Way.layers[i].blocks)
+                    {
+                        if (block.active_inputs != null)
+                            foreach (var input in block.active_inputs)
+                            {
+                                input_sum += Convert.ToInt32(input);
+                            }
+                    }
+                if (input_sum == 0)
+                {
+                    index = i - 1;
+                }
+            }
+            return index;
+        }
+        static void KLayer(SPNetWayBlock kblock)
+        {
+            kblock.active_outputs = kblock.active_inputs;
+        }
+        static void SLayer(SPNetWay Way, ISPNet Net, int LIndex, int SIndex, ref long MIN, long CurrentCor)
+        {
+            throw new NotImplementedException();
+            SPNetWayBlock WayBlock = Way.layers[LIndex].blocks[SIndex];
+            IBlock NetBlock = Net.GetLayers()[LIndex].GetBlock((byte)SIndex);
+            var States = NetBlock.ExtractStates(MIN, CurrentCor, WayConverter.ToByte(WayBlock.active_inputs));
+            foreach (var State in States)
+            {
+                var Outputs = WayConverter.ToList(State._outputs, WayBlock.active_outputs.Count);
+                WayBlock.active_outputs = Outputs;
+                CurrentCor *= State._cor;
+                Solver(Net, Way, ref MIN, CurrentCor);
+            }
+        }
+        static void PLayer()
+        {
+            throw new NotImplementedException();
+        }
+        public static void Solver(ISPNet Net, SPNetWay Way, ref long MIN, long CurrentCor)
+        {
+            throw new NotImplementedException();
+            var layersCount = Way.layers.Count();
+            var roundsCount = layersCount / 3;
+            #region FindLastNotEmptyLayer
+            int lastNotEmptyLayerIndex = SearchLastNotEmptyLayer(Way);
+            #endregion
+            #region FullRounds
+            if ((lastNotEmptyLayerIndex >= 0) && (lastNotEmptyLayerIndex / 3 < roundsCount - 1))
+            {
+                for (int i = lastNotEmptyLayerIndex / 3; i < roundsCount - 1; i++)
+                {
+                    #region K-layer
+                    if (lastNotEmptyLayerIndex % 3 == 0)
+                    {
+                        KLayer(Way.layers[lastNotEmptyLayerIndex].blocks[0]);
+                        lastNotEmptyLayerIndex++;
+                    }
+                    #endregion
+                    #region S-layer
+                    if (lastNotEmptyLayerIndex % 3 == 1)
+                    {
+                        for(int j=0;j<Way.layers[lastNotEmptyLayerIndex].blocks.Count;j++)
+                        {
+                            SLayer(Way, Net, lastNotEmptyLayerIndex, j, ref MIN, CurrentCor);
+                        }
+                        lastNotEmptyLayerIndex++;
+                    }
+                    #endregion
+                    #region P-layer
+                    if (lastNotEmptyLayerIndex % 3 == 2)
+                    {
+                        lastNotEmptyLayerIndex++;
+                    }
+                    #endregion
+                }
+            }
+            #endregion
+            #region LastRound
+            #endregion
+        }
     }
     public class Engine : IEngine
     {
