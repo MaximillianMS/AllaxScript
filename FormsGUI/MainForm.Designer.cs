3bf2f35d2a9b915d2223304af953c501bf7f6d38
TEST <TEST@TEST->
2017-06-25 23:15:33 +0300

    First stub functionality

--- FormsGUI/MainForm.Designer.cs
+++ FormsGUI/MainForm.Designer.cs
@@ -30,6 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sPNetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -37,9 +40,8 @@
             this.sLayerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.LeftPanel = new System.Windows.Forms.Panel();
             this.layersListBox = new System.Windows.Forms.ListBox();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.solutionsListBox = new System.Windows.Forms.ListBox();
+            this.graphVisualsPanel = new System.Windows.Forms.Panel();
             this.toolStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.LeftPanel.SuspendLayout();
@@ -56,65 +58,6 @@
             this.toolStrip1.Size = new System.Drawing.Size(784, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.addToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.sPNetToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-            this.fileToolStripMenuItem.Text = "Файл";
-            // 
-            // sPNetToolStripMenuItem
-            // 
-            this.sPNetToolStripMenuItem.Name = "sPNetToolStripMenuItem";
-            this.sPNetToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
-            this.sPNetToolStripMenuItem.Text = "Run";
-            this.sPNetToolStripMenuItem.Click += new System.EventHandler(this.sPNetToolStripMenuItem_Click);
-            // 
-            // addToolStripMenuItem
-            // 
-            this.addToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.sLayerToolStripMenuItem});
-            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
-            this.addToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
-            this.addToolStripMenuItem.Text = "Добавить";
-            // 
-            // sLayerToolStripMenuItem
-            // 
-            this.sLayerToolStripMenuItem.Name = "sLayerToolStripMenuItem";
-            this.sLayerToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
-            this.sLayerToolStripMenuItem.Text = "Round";
-            this.sLayerToolStripMenuItem.Click += new System.EventHandler(this.sLayerToolStripMenuItem_Click);
-            // 
-            // LeftPanel
-            // 
-            this.LeftPanel.Controls.Add(this.layersListBox);
-            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.LeftPanel.Location = new System.Drawing.Point(0, 49);
-            this.LeftPanel.Name = "LeftPanel";
-            this.LeftPanel.Size = new System.Drawing.Size(200, 363);
-            this.LeftPanel.TabIndex = 2;
-            // 
-            // layersListBox
-            // 
-            this.layersListBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.layersListBox.FormattingEnabled = true;
-            this.layersListBox.Location = new System.Drawing.Point(0, 0);
-            this.layersListBox.Name = "layersListBox";
-            this.layersListBox.Size = new System.Drawing.Size(200, 363);
-            this.layersListBox.TabIndex = 0;
             // 
             // toolStripButton1
             // 
@@ -143,11 +86,89 @@
             this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton3.Text = "toolStripButton3";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.addToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sPNetToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // sPNetToolStripMenuItem
+            // 
+            this.sPNetToolStripMenuItem.Name = "sPNetToolStripMenuItem";
+            this.sPNetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sPNetToolStripMenuItem.Text = "Run";
+            this.sPNetToolStripMenuItem.Click += new System.EventHandler(this.sPNetToolStripMenuItem_Click);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sLayerToolStripMenuItem});
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.addToolStripMenuItem.Text = "Добавить";
+            // 
+            // sLayerToolStripMenuItem
+            // 
+            this.sLayerToolStripMenuItem.Name = "sLayerToolStripMenuItem";
+            this.sLayerToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sLayerToolStripMenuItem.Text = "Round";
+            this.sLayerToolStripMenuItem.Click += new System.EventHandler(this.sLayerToolStripMenuItem_Click);
+            // 
+            // LeftPanel
+            // 
+            this.LeftPanel.Controls.Add(this.layersListBox);
+            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanel.Location = new System.Drawing.Point(0, 49);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(200, 363);
+            this.LeftPanel.TabIndex = 2;
+            // 
+            // layersListBox
+            // 
+            this.layersListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layersListBox.FormattingEnabled = true;
+            this.layersListBox.Location = new System.Drawing.Point(0, 0);
+            this.layersListBox.Name = "layersListBox";
+            this.layersListBox.Size = new System.Drawing.Size(200, 363);
+            this.layersListBox.TabIndex = 0;
+            // 
+            // solutionsListBox
+            // 
+            this.solutionsListBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.solutionsListBox.FormattingEnabled = true;
+            this.solutionsListBox.Location = new System.Drawing.Point(638, 49);
+            this.solutionsListBox.Name = "solutionsListBox";
+            this.solutionsListBox.Size = new System.Drawing.Size(146, 363);
+            this.solutionsListBox.TabIndex = 3;
+            // 
+            // graphVisualsPanel
+            // 
+            this.graphVisualsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.graphVisualsPanel.Location = new System.Drawing.Point(200, 49);
+            this.graphVisualsPanel.Name = "graphVisualsPanel";
+            this.graphVisualsPanel.Size = new System.Drawing.Size(438, 363);
+            this.graphVisualsPanel.TabIndex = 4;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 412);
+            this.Controls.Add(this.graphVisualsPanel);
+            this.Controls.Add(this.solutionsListBox);
             this.Controls.Add(this.LeftPanel);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
@@ -177,6 +198,8 @@
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ListBox solutionsListBox;
+        private System.Windows.Forms.Panel graphVisualsPanel;
     }
 }
 
--- FormsGUI/MainForm.cs
+++ FormsGUI/MainForm.cs
@@ -17,29 +17,49 @@ namespace FormsGUI
         Engine e;
         ISPNet net;
         List<string> layerList = new List<string>();
+        List<Solution> solutions = new List<Solution>();
         public MainForm()
         {
+            InitializeComponent();
             e = new Engine();
             e.ONSOLUTIONFOUND += this.E_AddSolution;
-            InitializeComponent();
+            e.ONPROGRESSCHANGED += this.E_ProgressChanged;
+            e.ONTASKDONE += E_OnTaskDone;
+            e.ONALLTASKSDONE += E_OnAllTasksDone;
+            
             SPNetSettings settings = new SPNetSettings(16, 4);
             net = e.CreateSPNetInstance(settings);
+            addFullRound();
+            addLastRound();
         }
 
+        private void E_OnTaskDone(Allax.Task T)
+        {
+            //int i = 0;
+        }
+        private void E_OnAllTasksDone(Allax.Task T)
+        {
+            MessageBox.Show("All done!");
+        }
         public void E_AddSolution(Solution s)
         {
-            MessageBox.Show("Solution found " + s.P);
+            MessageBox.Show("Solution found " + s.P.ToPrevalence());
         }
-        private void addRound()
+        public void E_ProgressChanged(double progress)
+        {
+            //double Progress = progress;
+        }
+        private void addFullRound()
         {
             net.AddLayer(LayerType.KLayer);
             net.AddLayer(LayerType.SLayer);
             net.AddLayer(LayerType.PLayer);
             var PBlockInit = new List<byte> { 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16 };
+            var SBlockInit = new List<byte> { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 };
             int layer = net.GetLayers().Count - 1;
             InitPLayer(layer, PBlockInit, net);
             InitSLayer(layer - 1, SBlockInit, net);
-            refreshList();
+            //refreshList();
 
         }
         private void addLastRound()
@@ -48,6 +68,13 @@ namespace FormsGUI
             net.AddLayer(LayerType.SLayer);
             net.AddLayer(LayerType.KLayer);
             refreshList();
+        }
+
+        private void addRound()
+        {
+            DelLastRound(net);
+            addFullRound();
+            addLastRound();
         }
 
         static void InitPLayer(int L, List<byte> PBlockInit, ISPNet Net)
@@ -100,27 +127,34 @@ namespace FormsGUI
         private void sLayerToolStripMenuItem_Click(object sender, EventArgs e)
         {
             EditSBlock f = new EditSBlock();
-            f.ShowDialog();
+            //f.ShowDialog();
             addRound();
-            int size = net.GetLayers().Count;
-            foreach (IBlock b in net.GetLayers()[size - 2].GetBlocks())
+            //int size = net.GetLayers().Count;
+            /*foreach (IBlock b in net.GetLayers()[size - 2].GetBlocks())
             {
                 b.Init(f.SValues);
-            }
+            }*/
         }
         
         private void runAnalysis()
         {
-            var R1 = new Allax.Rule(AvailableSolverTypes.);
-            var R2 = new Allax.Rule(AvailableSolverTypes.HeuristicSolver);
-            var F = new Allax.ADDSOLUTIONHANDLER(AddSolution);
-            //var AP = new AnalisysParams(new Algorithm(new List<Allax.Rule> { R1, R2 }, AnalisysType.Linear), F);
-            //net.PerformAnalisys(AP);
+            var R1 = new Allax.Rule(AvailableSolverTypes.BruteforceSolver);
+            var R2 = new Allax.Rule(AvailableSolverTypes.GreedySolver);
+            var R3 = new Allax.Rule(AvailableSolverTypes.AdvancedSolver);
+            //var F = new Allax.ADDSOLUTIONHANDLER(AddSolution);
+            var AP = new AnalisysParams(new Algorithm(new List<Allax.Rule> { R1, R2, R3}, AnalisysType.Linear));
+            e.PerformAnalisys(AP);  
         }
 
         private void sPNetToolStripMenuItem_Click(object sender, EventArgs e)
         {
             runAnalysis();
+            solutionsListBox.Items.Clear();
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            addRound();
         }
     }
 }
