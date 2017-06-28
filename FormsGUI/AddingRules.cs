8f781517b940de80b46dec0337b300bc5ef3d0b4
Maximillian <maxim00795@mail.ru>
2017-06-28 22:00:38 +0300

    New add rules form

---
+++ FormsGUI/AddingRules.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsGUI
+{
+    public partial class AddingRules : Form
+    {
+        public AddingRules()
+        {
+            InitializeComponent();
+        }
+    }
+}
